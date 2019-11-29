1684c1f14a5f5e0fbf8f771fa2b624a118ba7ce5
Sam Robichaud <Snrobichaud@gmail.com>
2019-11-29 14:52:31 -0400

    First Commit

---
+++ Assets/TimelineTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        timeline.Play();
+        Debug.Log("YEAH!");
+
+        if (OneTimeTrigger == true)
+        {
+        Destroy(this.gameObject);
+        }        
+    }
+
+}
